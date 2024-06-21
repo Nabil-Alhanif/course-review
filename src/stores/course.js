53bc8f32b644f8781efd0f2f41de08d232d5c67d
Nabil-Alhanif <nabil.alhanif@protonmail.com>
2024-06-21 16:32:03 -0700

    Instal 'axios' and 'ajv' for API fetch and JSON validation

--- src/stores/course.js
+++ src/stores/course.js
@@ -1,12 +1,37 @@
+import Ajv from 'ajv'
+import axios from 'axios'
 import { defineStore } from 'pinia'
-import { ref } from 'vue'
+
+const API_URL =
+	'https://script.google.com/macros/s/AKfycbzC-fCdBpr00Qt7Dy2w6u66qEDQ5GDKUXEagKeSjpKF5Ex_WELXqjhbq-xPyjCA-ty3ug/exec'
+
+const courseSchema = {
+	type: 'object',
+	properties: {
+		id: { type: 'string' },
+		faculty: { type: 'string' },
+		code: { type: 'string' },
+		title: { type: 'string' }
+	},
+	required: ['id', 'faculty', 'code', 'title']
+}
+
+const multiCourseSchema = {
+	type: 'array',
+	items: courseSchema
+}
+
+const ajv = new Ajv()
+const validateCourse = ajv.compile(courseSchema)
+const validateMultiCourse = ajv.compile(multiCourseSchema)
 
 export const useCourseStore = defineStore({
 	id: 'course',
 	state: () => ({
-		courses: ref([]),
-		loading: ref(false),
-		error: ref(null)
+		course: null,
+		courses: [],
+		loading: false,
+		error: null
 	}),
 	getters: {
 		//
@@ -15,11 +40,45 @@ export const useCourseStore = defineStore({
 		async fetchCourses() {
 			this.courses = []
 			this.loading = true
+			this.error = null
 
 			try {
-				this.courses = await fetch(
-					'https://script.google.com/macros/s/AKfycbzC-fCdBpr00Qt7Dy2w6u66qEDQ5GDKUXEagKeSjpKF5Ex_WELXqjhbq-xPyjCA-ty3ug/exec?action=getSheetData&sheetName=Courses'
-				).then((response) => response.json())
+				const response = await axios.get(API_URL, {
+					params: { action: 'getCourses' }
+				})
+
+				// Validate the response data against the schema
+				if (!validateMultiCourse(response.data)) {
+					throw new Error('Invalid response format')
+				}
+
+				this.courses = response.data.sort((a, b) => a.code.localeCompare(b.code))
+			} catch (error) {
+				this.error = error
+			} finally {
+				this.loading = false
+			}
+		},
+
+		async fetchCourseById(id) {
+			this.course = null
+			this.loading = true
+			this.error = null
+
+			try {
+				const response = await axios.get(API_URL, {
+					params: {
+						action: 'getCourseById',
+						targetId: id
+					}
+				})
+
+				// Validate the response data against the schema
+				if (!validateCourse(response.data)) {
+					throw new Error('Invalid response format')
+				}
+
+				this.course = response.data
 			} catch (error) {
 				this.error = error
 			} finally {
