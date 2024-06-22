92488bf352a97f61bbe1955d74f667c4494e7d1f
adhgn <adhigunareyhan@gmail.com>
2024-06-21 18:19:35 -0700

    tambah dikit

--- src/components/ReviewCard.vue
+++ src/components/ReviewCard.vue
@@ -1,7 +1,47 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+
+const props = defineProps({
+	course: {
+		type: Object,
+		default: () => ({
+			faculty: 'Unknown Faculty',
+			code: 'Unknown Code',
+			title: 'Unknown Title'
+		}),
+		required: true
+	},
+	review: {
+		type: Object,
+		default: () => ({
+			id: '0',
+			user_id: '0',
+			course_id: '0',
+			instructor_id: '0',
+			instructor_name: 'Unknown Instructor',
+			reviewer_faculty: 'Unknown Faculty',
+			reviewer_standing: 'Unknown Standing',
+			instructor_rating: 1,
+			workload: 1,
+			difficulties: 1,
+			recommended: 'Yes',
+			description: 'No description',
+			tips: 'No tips',
+			timestamp: '-'
+		})
+	}
+})
+</script>
+
 <template>
 	<div class="bg-white rounded-lg shadow-lg dark:bg-gray-100">
-		<div class="p-5">
-      <div class="mt-4 flex  justify-center  items-center">
+		<div class="p-5 flex">
+			<div class="w-9/12 mr-3">
+			<div class="flex">
+				<div class="w-3/12"><a href="#">
+					<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">{{ review.instructor_name }}</h5>
+				</a></div>
+				<div class="mb-2 flex  items-center w-3/12">
 								<svg
 									class="w-4 h-4 text-yellow-300 me-1"
 									aria-hidden="true"
@@ -59,9 +99,9 @@
 								</svg>
 
 							</div>
-			<a href="#">
-				<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">Dany hmm</h5>
-			</a>
+				<div class="mb-2 w-2/12 font-semibold">Difficulty: {{ review.difficulties }}</div>
+				<div class="mb-2 w-2/12 font-semibold">Workload: {{ review.workload }}</div>
+			</div>
       
 			<p class="mb-3 font-normal text-gray-700 dark:text-gray-700">
 				the prof is the shit! gotta register asap dude, you wont regret it but idk tho its math
@@ -69,7 +109,30 @@
 				ergergerg efrerfwd awefawefwefw fearferf eargferg erferf erfea rfearf aerf aerx
 			</p>
 
-      <p> [ some other stat ] </p>
+      		<p> [ some other stat ]</p>
+			</div>
+
+			<div>
+				<div class="object-center">
+					<h5 class="font-bold text-gray-900 mb-2 text-xl">Reviewer Info:</h5>
+					<div>
+						<table class="flex-col">
+							<tr>
+								<td>Faculty:</td>
+								<td>{{ review.reviewer_faculty }}</td>
+							</tr>
+							<tr>
+								<td>Standing:</td>
+								<td>{{ review.reviewer_standing }}</td>
+							</tr>
+							<tr>
+								<td>Recommended:</td>
+								<td>{{ review.recommended }}</td>
+							</tr>
+						</table>
+			</div>
+			</div>
+			</div>
 		</div>
 	</div>
 </template>
